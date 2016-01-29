5da98a9ff078f28e1cd18a3b60c2c45410e0a692
Hydraulicus <852gin@gmail.com>
2016-01-29 16:04:56 +0200

    2

--- ekalan/js/abacus.js
+++ ekalan/js/abacus.js
@@ -8,69 +8,111 @@ var TableTop = {//prototipe
 		this.aD= aD;//array of N dragable sides [2,3]
 		this.S = 0; //area in sq m
 
-		//this.dragLine = [] //coords of dragable borders. It is two border
-
+		console.dir(wholeTable.S);
 
 		  Object.defineProperty(this, "S", {// S will be calculated automaticaly everytime
 		    get: function() {
 		      return this.W * this.L * 0.000001; //area in sq m
 		    }
 		  });
-
-		  // Object.defineProperty(this, "dragLine", {// coords of dragable borders
-		  //   get: function() {console.log(this);
-		  //   	var arr= new Array();
-
-				// 		arr.push({x : this.L, y : 0, x1 : this.L, y1 : this.W, cur : 'e-resize' });
-				// 		arr.push({x : this.L, y : this.W, x1 : 0, y1 : this.W, cur : 'n-resize' });
-
-				// return arr
-		  //   }
-		  // });
-
-
 		return this
 	},
-
-
-
-}//end of TableTop
-
+}//end of TableTop constructor
 
 var wholeTable = {
 				S : [],
 		array4Drag: [],		//array which contain coords of dragable lines
 		dragLines : [],		//array which contain dragable lines with coord from array4Drag. All this lines are redrawing afte draging
+		underWins : [], 	// array which contain coords of windowsills
+	  windowsills : [], 	// array which contain windowsills
+
 		// tableCountur : ''
-	}
+			aC : [],//array of coord of circle which show dblclick
+			circles : [],//array of circle which show dblclick
+	baseLofunderWin : 800, //defoult L of windowsill
+	};
 Object.defineProperty(wholeTable, "tableCountur", {
-	set: function() {console.log('wholeTable setted!');},
     get: function() {
-    	var d='M0,0'+' h'+this.S[0].L
-		+' v'+(this.S[0].W+this.S[1].L)+' h'+'-'+this.S[1].W+' v'+'-'+this.S[1].L //concern first wing
-		+' h'+'-'+(this.S[0].L-this.S[1].W-this.S[2].W)
-		+' v'+this.S[2].L+' h'+'-'+this.S[2].W+' v'+'-'+(this.S[2].L+this.S[0].W) //concern second wing
-		+'z';
+    	var
+	    	L1 = (typeof wholeTable.S[1] !== 'undefined')? this.S[1].L : 0,
+	    	W1 = (typeof wholeTable.S[1] !== 'undefined')? this.S[1].W : 0,
+	    	L2 = (typeof wholeTable.S[2] !== 'undefined')? this.S[2].L : 0,
+	    	W2 = (typeof wholeTable.S[2] !== 'undefined')? this.S[2].W : 0,
+
+	    	d='M0,0'+' h'+this.S[0].L
+				+' v'+(this.S[0].W+L1)+' h'+'-'+W1+' v'+'-'+L1 //concern first wing
+				+' h'+'-'+(this.S[0].L-W1-W2)
+				+' v'+L2+' h'+'-'+W2+' v'+'-'+(L2+this.S[0].W) //concern second wing
+				+'z';
 		return d
+	    }
+});
+
+// Object.defineProperty(wholeTable, "S", {
+// 	set: function() {
+// 		// console.log('wholeTable setted!');
+// 	}
+  // });
+//
+    // };
+
+		  Object.defineProperty(wholeTable, "aC", {//array of coord of circle which show dblclick
+		    get: function() {
+		    	var arr= new Array();
+						if ( typeof wholeTable.S[0] !== 'undefined') {
+								arr.push({x : wholeTable.S[0].L*0.5, y : 0});
+								arr.push({x : wholeTable.S[0].L*0.5, y : wholeTable.S[0].W });
+							}
+						if ( typeof wholeTable.S[1] !== 'undefined') {
+								arr.push({x : wholeTable.S[0].L, y : wholeTable.S[1].L*0.5});
+								arr.push({x : wholeTable.S[1].X0-wholeTable.S[1].W, y : wholeTable.S[1].Y0+wholeTable.S[1].L*0.5});
+								arr.push({x : wholeTable.S[1].X0-wholeTable.S[1].W*0.5, y : wholeTable.S[1].Y0+wholeTable.S[1].L});
+							}
+						if ( typeof wholeTable.S[2] !== 'undefined') {
+								arr.push({x : wholeTable.S[2].X0+wholeTable.S[2].W, y : wholeTable.S[2].Y0+wholeTable.S[2].L*0.5, });
+								arr.push({x : wholeTable.S[2].X0+wholeTable.S[2].W*0.5, y : wholeTable.S[2].Y0+wholeTable.S[2].L, });
+								arr.push({x : wholeTable.S[2].X0, y : wholeTable.S[2].Y0+wholeTable.S[2].L*0.5});
+							}
+		    	console.log('array of coord of circle which show dblclick recreated!');
+				return arr
+		    }
+		  });
+Object.defineProperty(wholeTable, "underWins", { //generation array of coord of underWins
+    get: function() {
+    	var
+	    	L1 = (typeof wholeTable.S[1] !== 'undefined')? this.S[1].L : 0,
+	    	W1 = (typeof wholeTable.S[1] !== 'undefined')? this.S[1].W : 0,
+	    	L2 = (typeof wholeTable.S[2] !== 'undefined')? this.S[2].L : 0,
+	    	W2 = (typeof wholeTable.S[2] !== 'undefined')? this.S[2].W : 0,
+	    	hl = wholeTable.baseLofunderWin * 0.5;
+    	var arr= new Array();
+				if ( typeof wholeTable.S[0] !== 'undefined') {
+						arr.push({x : wholeTable.S[0].L, y : L1*0.5-hl});
+						arr.push({x : wholeTable.S[0].L*0.5, y : wholeTable.S[0].W });
+					}
+				if ( typeof wholeTable.S[1] !== 'undefined') {
+						arr.push({x : wholeTable.S[1].X0-wholeTable.S[1].W, y : wholeTable.S[1].Y0+wholeTable.S[1].L*0.5});
+						arr.push({x : wholeTable.S[1].X0-wholeTable.S[1].W*0.5, y : wholeTable.S[1].Y0+wholeTable.S[1].L});
+					}
+				if ( typeof wholeTable.S[2] !== 'undefined') {
+						arr.push({x : wholeTable.S[2].X0+wholeTable.S[2].W*0.5, y : wholeTable.S[2].Y0+wholeTable.S[2].L, });
+						arr.push({x : wholeTable.S[2].X0, y : wholeTable.S[2].Y0+wholeTable.S[2].L*0.5});
+					}
+    	return arr
 	    }
     });
 
-
-// Object.defineProperty(wholeTable, "array4Drag", { //generation array of coord of gragablel lines, type of cursor, data that will be changed
-//     get: function() {
-//     });
-
 Object.defineProperty(wholeTable, "array4Drag", {
 	set: function(){
-		console.log('array4Drag renewed!');
+		console.log('array4Drag should be renewed!');
 	},
     get: function() { //generation array of coord of gragablel lines, type of cursor, data that will be changed
-    	var arr= new Array(), obj={}, j=0;
+    	var arr= new Array();
     	console.log('array4Drag of coords recreated!');
     	// for (var i in this.S) {
-
+    	var	L1 = (typeof wholeTable.S[1] !== 'undefined')? this.S[1].L : 0;
 				if ( typeof wholeTable.S[0] !== 'undefined') {
-						arr.push({x : wholeTable.S[0].L, y : 0, x1 : wholeTable.S[0].L, y1 : wholeTable.S[0].W, cur : 'e-resize', infl2 : 'L', infl4 : '0'});
+						arr.push({x : wholeTable.S[0].L, y : 0, x1 : wholeTable.S[0].L, y1 : wholeTable.S[0].W+L1, cur : 'e-resize', infl2 : 'L', infl4 : '0'});
 						arr.push({x : wholeTable.S[0].L, y : wholeTable.S[0].W, x1 : 0, y1 : wholeTable.S[0].W, cur : 'n-resize', infl2 : 'W', infl4 : '0' });
 					}
 				if ( typeof wholeTable.S[1] !== 'undefined') {
@@ -97,51 +139,57 @@ var graphModel ={
 drawDragCountuor : function(canva, array4Drag, matrix){//Start draw dragable element
 			for (var i in array4Drag) {//console.log('Start draw dragable element N '+i);
 					wholeTable.dragLines[i]  = canva.line( array4Drag[i].x, array4Drag[i].y, array4Drag[i].x1, array4Drag[i].y1)
-									.attr({ stroke: '#888', 'strokeWidth': 40,cursor: array4Drag[i].cur, id:array4Drag[i].infl2})
+									.attr({ stroke: 'transparent', 'strokeWidth': 40,cursor: array4Drag[i].cur, id:array4Drag[i].infl2})
 									.data('infl2',array4Drag[i].infl2)
 									.data('infl4',array4Drag[i].infl4)
 									 .drag( dragMove , start, stopE)
-									 .hover(function() {
-									 	this.attr({strokeWidth: 30,stroke:"blue"});
-									 }, function() {
-									 	this.attr({strokeWidth: 40,stroke:"#888"});
-									 });
+									 .hover(function() { this.attr({strokeWidth: 30,stroke:"#aaf"}) },
+										 	function() { this.attr({strokeWidth: 40,stroke:"transparent"}) })
+									 .dblclick(underWinGrow);
 
 					if (typeof matrix !== 'undefined') {wholeTable.dragLines[i].transform(matrix) }
 				}
 				return wholeTable.dragLines
 		},
 //===========================================================================================
+
+
 reDrawDragCountuor : function(canva, array4Drag, matrix){//Start draw dragable element
-					// wholeTable.dragLines[1].attr({ x1:array4Drag[1].x, y1:array4Drag[1].y, x2:array4Drag[1].x1, y2:array4Drag[1].y1})
 			for (var i in array4Drag) {
-					// console.log('Start redraw dragable element N '+i);
-					// console.log(wholeTable.dragLines[i].attr('y1'));
-					wholeTable.dragLines[i].attr({ x1:array4Drag[i].x, y1:array4Drag[i].y, x2:array4Drag[i].x1, y2:array4Drag[i].y1});
-					// console.log(wholeTable.dragLines[i].attr('y1'));
+					if (typeof wholeTable.dragLines[i] !== 'undefined')
+						wholeTable.dragLines[i].attr({ x1:array4Drag[i].x, y1:array4Drag[i].y, x2:array4Drag[i].x1, y2:array4Drag[i].y1});
 				}
-		}
+		},
 
-//========================== It is work too !!!! =========================
-//drawDragCountuor : function(canva, matrix){//Start draw dragable element
-// 			var l=[];
-// 			var icol = ['red','gold','green'];
-// 			for (var j in wholeTable.S) {console.log('Start draw dragable element N '+j);
-// 				var arr = wholeTable.S[j].dragLine;
-// 				for (var i in arr)
-// 					l[i]  = canva.line( arr[i].x, arr[i].y, arr[i].x1, arr[i].y1)
-// 									.attr({ stroke: icol[j], 'strokeWidth': 40,cursor: arr[i].cur})
-// 									.data('vertex',i)
-// 									 .drag( dragMove , start, stopE)
-// 									 .hover(function() {
-// 									 	this.attr({strokeWidth: 50,stroke:"blue"});
-// 									 }, function() {
-// 									 	this.attr({strokeWidth: 40,stroke:"magenta"});
-// 									 })
-// 									.transform(matrix);
-// 				}
-// 		}
+reDrawCircles : function(canva, array4, matrix){//redraw dblclick cicrcles
+			for (var i in array4) {//console.log('Start draw cercles  N '+i);
+					wholeTable.circles[i].attr({ cx : array4[i].x, cy : array4[i].y});
+				}
+		},
 
+drawCircles : function(canva, array4, matrix){//Start draw dblclick cicrcles
+	// console.dir(array4);
+	if (typeof wholeTable.circles !== 'undefined') { for (var i in wholeTable.circles) wholeTable.circles[i].remove()};
+			for (var i in array4) {//console.log('Start draw dragable element N '+i);
+					wholeTable.circles[i]  = canva.circle( array4[i].x, array4[i].y, 30)
+									.attr({ stroke: 'transparent', strokeWidth: 5, fill : '#bbb'});
+					if (typeof matrix !== 'undefined') {wholeTable.circles[i].transform(matrix) }
+						// wholeTable.circlesGroup.add(wholeTable.circles[i]);
+				}
+			// console.dir(wholeTable.circles);
+		return wholeTable.circles
+		},
+
+drawWindowSills : function(n, array4, matrix){//Start draw WindowSills
+	// console.dir(array4);
+
+					wholeTable.windowsills[n]  = canva.rect( array4[n].x, array4[n].y, 30)
+									.attr({ stroke: 'transparent', strokeWidth: 5, fill : '#bbb'});
+					if (typeof matrix !== 'undefined') {wholeTable.circles[i].transform(matrix) }
+						wholeTable.circlesGroup.add(wholeTable.circles[i]);
+
+				return wholeTable.circles
+		},
 
 }// end graphModel object
 
@@ -157,39 +205,62 @@ reDrawDragCountuor : function(canva, array4Drag, matrix){//Start draw dragable e
 function initialisation(){
 	var myMatrix = new Snap.Matrix();
 	myMatrix.scale(0.8,0.4);            // play with scaling before and after the rotate
-	myMatrix.translate(800,100);      // this translate will not be applied to the rotation
+	// myMatrix.translate(100,100);      // this translate will not be applied to the rotation
 	myMatrix.rotate(45);            // rotate
-
-	wholeTable.S[0]=Object.create(TableTop).constructor(0, 0, 600, 2500, [2,3]);
-	wholeTable.S[1]=Object.create(TableTop).constructor(wholeTable.S[0].L, wholeTable.S[0].W, 450, 2500, [2,3]);
-	wholeTable.S[2]=Object.create(TableTop).constructor(0, wholeTable.S[0].W, 600,1200, [2,3]);
-	console.dir(wholeTable.S);
-
-
 	graphModel.views.izometr = Snap("#svgout");//connect first viewport with izometric view
 	graphModel.views.front = Snap("#frontView");//connect 2-nd viewport with front view
+	console.log(graphModel.views.izometr);
 
+	wholeTable.S[0]=Object.create(TableTop).constructor(0, 0, 600, 2500, [2,3]);
+	// wholeTable.S[1]=Object.create(TableTop).constructor(wholeTable.S[0].L, wholeTable.S[0].W, 450, 2500, [2,3]);
+	// wholeTable.S[2]=Object.create(TableTop).constructor(0, wholeTable.S[0].W, 600,1200, [2,3]);
 
-	mainCountur.izometr=graphModel.views.izometr.path( wholeTable.tableCountur).attr({ stroke: '#88f', 'strokeWidth': 20,fill:"#999"}).transform(myMatrix);
-	mainCountur.front=graphModel.views.front.path( wholeTable.tableCountur).attr({ stroke: '#88f', 'strokeWidth': 20,fill:"#999"});//.animate({fill: 'red'}, 4000);
+	mainCountur.izometr=graphModel.views.izometr.path( wholeTable.tableCountur).attr({ stroke: '#999', 'strokeWidth': 20,fill:"#aaa"}).transform(myMatrix);
+	mainCountur.front=graphModel.views.front.path( wholeTable.tableCountur).attr({ stroke: '#aaa', 'strokeWidth': 20,fill:"#bbb"})
+											 .hover(function() {
+											 	this.attr({fill:"#bbc",stroke:"#aac"});
+											 }, function() {
+											 	this.attr({fill:"#bbb",stroke: '#aaa',});
+											 })
+											 .mouseover(twoWawe)
+											 ;//.animate({fill: 'red'}, 4000);
 
-			 zoom0(graphModel.views.front);
-			 zoom0(graphModel.views.izometr);
-
-	console.log(wholeTable.array4Drag);
 
 	graphModel.drawDragCountuor(graphModel.views.front, wholeTable.array4Drag);
 	// graphModel.drawDragCountuor(graphModel.views.front);
 	// console.log(wholeTable.dragLines[0].attr('y1'));
 
+	graphModel.drawCircles(graphModel.views.front, wholeTable.aC);
+	zoom0(graphModel.views.front);
+	zoom0(graphModel.views.izometr);
+
+}
+function underWinGrow(){
+	console.log('dblclick '+this);
+	var rec;
+
+	wholeTable.underWins.push();
 }
 
+function twoWawe(){
+	console.clear();
+	for (var i in wholeTable.circles) {
+		wholeTable.circles[i].animate({stroke:'gold',strokeWidth:100,opacity:0},250, mina.easein
+			, function(){
+				this.attr({ stroke: '#fff', 'strokeWidth': 20,fill:"#bbb",opacity:1});
+				this.animate({stroke:'gold',strokeWidth:100,opacity:0},350, mina.easein
+				, function(){this.attr({ stroke: '#bbb', 'strokeWidth': 10,fill:"#bbb",opacity:1})
+						})
+				})
+		}
+
+}
 
 function zoom0(canva){ //zoom all objects in viewbox
 	var bBox = canva.getBBox()
 	 // var scale= ' '+bBox.x+' '+bBox.y+' '+bBox.w+' ' +bBox.h;console.log(scale)
 	// canva.stop().animate({"viewBox": scale}, 1000, mina.bounce, function(){console.log(scale)});
-	canva.attr({"viewBox": ' '+bBox.x+' '+bBox.y+' '+bBox.w+' ' +bBox.h});
+	canva.attr({"viewBox": ' '+(bBox.x-300)+' '+(bBox.y-300)+' '+(bBox.w+600)+' ' +(bBox.h+600)});
 	// document.querySelector('#frontView').setAttribute("viewBox", ' '+bBox.x+' '+bBox.y+' '+bBox.w+' ' +bBox.h);
 }
 
@@ -224,12 +295,10 @@ function stopE(el){
 		mainCountur.front.animate({'d':wholeTable.tableCountur}, 750, mina.bounce, function(){
 								that.transform( oldMatrix	);
 								graphModel.reDrawDragCountuor(graphModel.views.front, wholeTable.array4Drag);
-								zoom0(graphModel.views.front);});
+								zoom0(graphModel.views.front);
+								graphModel.reDrawCircles(graphModel.views.front, wholeTable.aC);});
 		mainCountur.izometr.animate({'d':wholeTable.tableCountur}, 750, mina.elastic, function(){ zoom0(graphModel.views.izometr);});
-
 	};
-
-
 
 //===Get mouse position====
 var mousePos = undefined;
